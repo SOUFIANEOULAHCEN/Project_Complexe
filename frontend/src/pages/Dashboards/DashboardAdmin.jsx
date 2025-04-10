7e2fb72797d0fe4b3499c9f73e3aad35063f6229
SOUFIANEOULAHCEN <soufiane.oulahcen@gmail.com>
2025-04-10 12:00:44 +0100

    add shadcn

--- frontend/src/pages/Dashboards/DashboardAdmin.jsx
+++ frontend/src/pages/Dashboards/DashboardAdmin.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { Button } from "@/components/ui/button"
 const DashboardAdmin = () => {
   return (
     <div>
-      <h2>Welcome, Admin</h2>;
+      <h2>Welcome, Admin xx</h2>;
     </div>
   )
 }
